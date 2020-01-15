91ddc65c7bc6f6f89ecd02de431be73fede41260
MuggleLego <57736018+MuggleLego@users.noreply.github.com>
2020-01-15 20:15:09 +0800

    Create simple_loop.go

---
+++ Muggle and GO/simple_loop.go
@@ -0,0 +1,29 @@
+ackage main
+
+import "fmt"
+
+func main(){
+        test1()
+        test2()
+}
+
+//one loop
+func test1(){
+        var str string
+        var tmp string
+        for i:=0;i<5;i++ {
+                tmp+="A"
+                str+=tmp+"\n"
+        }
+        fmt.Println(str)
+}
+
+//two loops
+func test2(){
+        for i:=0;i<5;i++ {
+                for j:=0;j<=i;j++ {
+                        fmt.Printf("C")
+                }
+                fmt.Println()
+        }
+}
