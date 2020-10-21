113b557a21c9478d931cb06d0f97849d72d06967
MuggleLego <57736018+MuggleLego@users.noreply.github.com>
2020-10-21 14:34:53 +0800

    Create exercise_slice.go

---
+++ Muggle and GO/exercise_slice.go
@@ -0,0 +1,20 @@
+package main
+
+import "golang.org/x/tour/pic"
+
+func Pic(dx, dy int) [][]uint8 {
+	ret:=make([][]uint8,dy)
+	for i,_ := range ret{
+		ret[i]=make([]uint8,dx)
+	}
+	for i:=0;i<dy;i++ {
+		for j:=0;j<dx;j++{
+			ret[i][j]=uint8(i+j)
+		}
+	}
+	return ret
+}
+
+func main() {
+	pic.Show(Pic)
+}
