39d227f8963c99db305d6d56cb07b248c15f7e29
MuggleLego <57736018+MuggleLego@users.noreply.github.com>
2020-10-21 15:08:19 +0800

    Create exercise_stringer.go

---
+++ Muggle and GO/exercise_stringer.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+type IPAddr [4]byte
+
+func (h IPAddr) String() string {
+	return fmt.Sprintf("%v.%v.%v.%v", h[0], h[1], h[2], h[3])
+}
+func main() {
+	hosts := map[string]IPAddr{
+		"loopback":  {127, 0, 0, 1},
+		"googleDNS": {8, 8, 8, 9},
+	}
+	for name, ip := range hosts {
+		fmt.Printf("%v: %v\n", name, ip)
+	}
+}
