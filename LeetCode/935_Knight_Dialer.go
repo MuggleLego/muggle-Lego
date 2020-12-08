35694439edd9d06e44b4262f90c123a47630ec36
MuggleLego <57736018+MuggleLego@users.noreply.github.com>
2020-12-08 20:48:47 +0800

    Update 935_Knight_Dialer.go

--- LeetCode/935_Knight_Dialer.go
+++ LeetCode/935_Knight_Dialer.go
@@ -2,20 +2,23 @@ func knightDialer(n int) int {
     table := [][]int{{4,6},{6,8},{7,9},{4,8},{3,9,0},{},{1,7,0},{2,6},{1,3},{2,4}}
     dp:=make([][]int,10)
     for i:=0;i<10;i++{
-        dp[i]=append(dp[i],1)
+        dp[i]=append(dp[i],1,1)
     }
     for i:=1;i<n;i++{
+        for k:=0;k<10;k++{
+            dp[k][0]=dp[k][1]
+        }
         for j:=0;j<10;j++{
             var tmp int=0
             for _,v:=range table[j]{
-                tmp=(tmp+dp[v][i-1])%1000000007
+                tmp=(tmp+dp[v][0])%1000000007
             }
-            dp[j]=append(dp[j],tmp)
+            dp[j][1]=tmp
         }
     }
     var res int=0
     for i:=0;i<10;i++{
-        res=(dp[i][n-1]+res)%1000000007
+        res=(dp[i][1]+res)%1000000007
     }
     return res
 }
